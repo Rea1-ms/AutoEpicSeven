a484f3cf8670fb3a7eb09150a5666225b79d417c
Rea1-ms <127638245+Rea1-ms@users.noreply.github.com>
2026-02-09 15:39:17 +0800

    Chore: add ANNOUNCEMENT_CLOSE assets

--- tasks/login/assets/assets_login_maintenance.py
+++ tasks/login/assets/assets_login_maintenance.py
@@ -3,10 +3,10 @@ from module.base.button import Button, ButtonWrapper
 # This file was auto-generated, do not modify it manually. To generate:
 # ``` python -m dev_tools.button_extract ```
 
-ADVERTISE_CLOSE = ButtonWrapper(
-    name='ADVERTISE_CLOSE',
+ANNOUNCEMENT_CLOSE = ButtonWrapper(
+    name='ANNOUNCEMENT_CLOSE',
     share=Button(
-        file='./assets/share/login/maintenance/ADVERTISE_CLOSE.png',
+        file='./assets/share/login/maintenance/ANNOUNCEMENT_CLOSE.png',
         area=(1221, 61, 1242, 82),
         search=(1201, 41, 1262, 102),
         color=(86, 91, 99),
