af5817309af3182d770f939708b18e4f856a6516
Rea1-ms <127638245+Rea1-ms@users.noreply.github.com>
2026-02-15 19:54:26 +0800

    Add: simply implemented sanctuary func

---
+++ tasks/sanctuary/assets/assets_sanctuary_forest_of_elves.py
@@ -0,0 +1,35 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+ALTAR_OF_GROWTH = ButtonWrapper(
+    name='ALTAR_OF_GROWTH',
+    share=Button(
+        file='./assets/share/sanctuary/forest_of_elves/ALTAR_OF_GROWTH.png',
+        area=(952, 632, 981, 663),
+        search=(932, 612, 1001, 683),
+        color=(160, 153, 149),
+        button=(952, 632, 981, 663),
+    ),
+)
+CARE = ButtonWrapper(
+    name='CARE',
+    share=Button(
+        file='./assets/share/sanctuary/forest_of_elves/CARE.png',
+        area=(1084, 545, 1109, 565),
+        search=(1064, 525, 1129, 585),
+        color=(100, 122, 103),
+        button=(1084, 545, 1109, 565),
+    ),
+)
+CLAIM_REWARDS = ButtonWrapper(
+    name='CLAIM_REWARDS',
+    share=Button(
+        file='./assets/share/sanctuary/forest_of_elves/CLAIM_REWARDS.png',
+        area=(924, 216, 934, 224),
+        search=(920, 211, 938, 461),
+        color=(223, 87, 11),
+        button=(924, 216, 934, 224),
+    ),
+)
---
+++ tasks/sanctuary/assets/assets_sanctuary_heart_of_eulerbis.py
@@ -0,0 +1,55 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+CUSTODY = ButtonWrapper(
+    name='CUSTODY',
+    share=Button(
+        file='./assets/share/sanctuary/heart_of_eulerbis/CUSTODY.png',
+        area=(589, 646, 607, 668),
+        search=(569, 626, 627, 688),
+        color=(67, 71, 81),
+        button=(589, 646, 607, 668),
+    ),
+)
+DEPOSIT_BOX_NOT_FULL = ButtonWrapper(
+    name='DEPOSIT_BOX_NOT_FULL',
+    share=Button(
+        file='./assets/share/sanctuary/heart_of_eulerbis/DEPOSIT_BOX_NOT_FULL.png',
+        area=(1179, 327, 1225, 370),
+        search=(1159, 307, 1245, 390),
+        color=(28, 40, 56),
+        button=(1179, 327, 1225, 370),
+    ),
+)
+PURIFY = ButtonWrapper(
+    name='PURIFY',
+    share=Button(
+        file='./assets/share/sanctuary/heart_of_eulerbis/PURIFY.png',
+        area=(134, 638, 168, 677),
+        search=(114, 618, 188, 697),
+        color=(32, 49, 36),
+        button=(134, 638, 168, 677),
+    ),
+)
+REWARDS_TIER_A = ButtonWrapper(
+    name='REWARDS_TIER_A',
+    share=Button(
+        file='./assets/share/sanctuary/heart_of_eulerbis/REWARDS_TIER_A.png',
+        area=(359, 398, 397, 429),
+        search=(339, 378, 417, 449),
+        color=(62, 34, 85),
+        button=(359, 398, 397, 429),
+    ),
+)
+REWARDS_TIER_B = ButtonWrapper(
+    name='REWARDS_TIER_B',
+    share=Button(
+        file='./assets/share/sanctuary/heart_of_eulerbis/REWARDS_TIER_B.png',
+        area=(363, 398, 394, 429),
+        search=(343, 378, 414, 449),
+        color=(45, 89, 142),
+        button=(363, 398, 394, 429),
+    ),
+)
