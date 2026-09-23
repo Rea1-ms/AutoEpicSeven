873a2f8685fd15b2658fc7947366debac65686cd
Rea1-ms <127638245+Rea1-ms@users.noreply.github.com>
2026-09-23 23:04:09 +0800

    Upd: add another COMMON_ACTIVITY_CHECK assets for event expiring soon state

--- tasks/activity/assets/assets_activity_common.py
+++ tasks/activity/assets/assets_activity_common.py
@@ -21,13 +21,22 @@ COMMON_ACTIVITY_CHECK = ButtonWrapper(
             button=(1064, 613, 1167, 633),
         ),
     ],
-    share=Button(
-        file='./assets/share/activity/common/COMMON_ACTIVITY_CHECK.png',
-        area=(1020, 34, 1034, 54),
-        search=(1009, 24, 1230, 59),
-        color=(194, 194, 195),
-        button=(1020, 34, 1034, 54),
-    ),
+    share=[
+        Button(
+            file='./assets/share/activity/common/COMMON_ACTIVITY_CHECK.png',
+            area=(1020, 34, 1034, 54),
+            search=(1009, 24, 1230, 59),
+            color=(194, 194, 195),
+            button=(1020, 34, 1034, 54),
+        ),
+        Button(
+            file='./assets/share/activity/common/COMMON_ACTIVITY_CHECK.2.png',
+            area=(1069, 36, 1082, 49),
+            search=(1009, 24, 1230, 59),
+            color=(209, 39, 43),
+            button=(1069, 36, 1082, 49),
+        ),
+    ],
 )
 COMMON_ACTIVITY_LIST = ButtonWrapper(
     name='COMMON_ACTIVITY_LIST',
